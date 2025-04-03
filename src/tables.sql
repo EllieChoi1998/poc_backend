0345907746794db72f3a77c80290ed2e0c6de68a
Ellie Choi <ellie19981008@gmail.com>
2025-04-03 17:52:57 +0900

    table 구조 수정 및 도커의 mysql 서버에 한글 설정

--- src/tables.sql
+++ src/tables.sql
@@ -17,11 +17,13 @@ CREATE TABLE user (
 
 CREATE TABLE document (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    document_name VARCHAR(100) NOT NULL,
     file_name VARCHAR(300) NOT NULL,
     embedding_id VARCHAR(100),
     doc_type VARCHAR(50) NOT NULL,
     user_id INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    current_state INT DEFAULT 0,
     FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE SET NULL
 );
 
@@ -31,8 +33,12 @@ CREATE TABLE serviceResult (
     service_name VARCHAR(100) NOT NULL,
     JSONFile_path VARCHAR(100),
     result_text TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    table_html_path VARCHAR(200),
     FOREIGN KEY (document_id) REFERENCES document(id) ON DELETE CASCADE
 );
 
 
+INSERT INTO team(name, department) VALUES ('컴플라이언스', '수탁사업부');
 INSERT INTO team(name, department) VALUES ('계약팀', '수탁사업부');
+INSERT INTO team(name, department) VALUES ('영업팀', '수탁사업부');
